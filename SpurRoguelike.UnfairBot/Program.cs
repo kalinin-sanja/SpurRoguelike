3c77d4db5c18c191559a2d42b7c32e17919b75cb
epeshk <1197228@gmail.com>
2018-02-14 15:41:07 +0500

    add UnfairBot template

---
+++ SpurRoguelike.UnfairBot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using SpurRoguelike.Core;
+using SpurRoguelike.Core.Primitives;
+using SpurRoguelike.Core.Views;
+
+namespace SpurRoguelike.UnfairBot
+{
+    public class UnfairBot : IPlayerController
+    {
+        public Turn MakeTurn(LevelView levelView, IMessageReporter messageReporter)
+        {
+            messageReporter.ReportMessage("Hey ho! I'm still breathing");
+
+            if (levelView.Random.NextDouble() < 0.1)
+                return Turn.None;
+
+            var nearbyMonster = levelView.Monsters.FirstOrDefault(m => IsInAttackRange(levelView.Player.Location, m.Location));
+
+            if (nearbyMonster.HasValue)
+                return Turn.Attack(nearbyMonster.Location - levelView.Player.Location);
+
+            return Turn.Step((StepDirection)levelView.Random.Next(4));
+        }
+
+        private static bool IsInAttackRange(Location a, Location b)
+        {
+            return a.IsInRange(b, 1);
+        }
+    }
+}
---
+++ SpurRoguelike.UnfairBot/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SpurRoguelike.UnfairBot")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpurRoguelike.UnfairBot")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("CAD3DD8E-4E5D-47E6-A34F-7D8EC863250F")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
