2392208e4a7067ec7210974837ecdb81b2f2639f
epeshk <peshkov@skbkontur.ru>
2018-03-08 16:11:40 +0500

    close holes in LevelView

--- SpurRoguelike.Core/Views/LevelView.cs
+++ SpurRoguelike.Core/Views/LevelView.cs
@@ -17,11 +17,11 @@ namespace SpurRoguelike.Core.Views
 
         public PawnView Player => level?.Player?.CreateView() ?? default(PawnView);
 
-        public IEnumerable<PawnView> Monsters => level?.Monsters.Select(m => m.CreateView());
+        public IEnumerable<PawnView> Monsters => level?.Monsters.Where(m => IsVisible(m.Location)).Select(m => m.CreateView());
 
-        public IEnumerable<ItemView> Items => level?.Items.Select(i => i.CreateView());
+        public IEnumerable<ItemView> Items => level?.Items.Where(m => IsVisible(m.Location)).Select(i => i.CreateView());
 
-        public IEnumerable<HealthPackView> HealthPacks => level?.HealthPacks.Select(hp => hp.CreateView());
+        public IEnumerable<HealthPackView> HealthPacks => level?.HealthPacks.Where(hp => IsVisible(hp.Location)).Select(hp => hp.CreateView());
 
         public Random Random => level?.Random;
 
@@ -29,17 +29,31 @@ namespace SpurRoguelike.Core.Views
 
         public PawnView GetMonsterAt(Location location)
         {
+            if (!IsVisible(location))
+                return default(PawnView);
             return level?.GetEntity<Monster>(location)?.CreateView() ?? default(PawnView);
         }
 
         public ItemView GetItemAt(Location location)
         {
+            if (!IsVisible(location))
+                return default(ItemView);
             return level?.GetEntity<Item>(location)?.CreateView() ?? default(ItemView);
         }
 
         public HealthPackView GetHealthPackAt(Location location)
         {
+            if (!IsVisible(location))
+                return default(HealthPackView);
             return level?.GetEntity<HealthPack>(location)?.CreateView() ?? default(HealthPackView);
+        }
+
+        private bool IsVisible(Location location)
+        {
+            if (!Player.HasValue)
+                return true;
+            var offset = (Player.Location - location).Abs();
+            return offset.XOffset <= level.Field.VisibilityWidth && offset.YOffset <= level.Field.VisibilityHeight;
         }
 
         private readonly Level level;
